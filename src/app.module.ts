87b8ac0eb0fd505ed51780a72c16bf2279e897ff
Jon Ganebski <jon.ganebski@gmail.com>
2021-01-05 13:07:37 +0900

    day2 making rest api

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PodcastsModule } from './podcast/podcasts.module';
 
 @Module({
-  imports: [],
+  imports: [PodcastsModule],
   controllers: [AppController],
   providers: [AppService],
 })
